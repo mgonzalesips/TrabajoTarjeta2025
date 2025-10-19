a9b59f5f7e1c995d5db35d8d775a21a513d8c844
valenll <luchini.valu@gmail.com>
2025-10-19 19:28:26 -0300

    Clases colectivo y boleto implementadas

---
+++ TarjetaSube/Boleto.cs
@@ -0,0 +1,42 @@
+﻿public class Boleto
+{
+    private decimal montoPagado;
+    private string lineaColectivo;
+    private string empresa;
+    private decimal saldoRestante;
+    private DateTime fechaHora;
+
+    public Boleto(decimal montoPagado, string lineaColectivo, string empresa, decimal saldoRestante)
+    {
+        this.montoPagado = montoPagado;
+        this.lineaColectivo = lineaColectivo;
+        this.empresa = empresa;
+        this.saldoRestante = saldoRestante;
+        this.fechaHora = DateTime.Now;
+    }
+
+    public decimal MontoPagado
+    {
+        get { return montoPagado; }
+    }
+
+    public string LineaColectivo
+    {
+        get { return lineaColectivo; }
+    }
+
+    public string Empresa
+    {
+        get { return empresa; }
+    }
+
+    public decimal SaldoRestante
+    {
+        get { return saldoRestante; }
+    }
+
+    public DateTime FechaHora
+    {
+        get { return fechaHora; }
+    }
+}
---
+++ TarjetaSube/Colectivo.cs
@@ -0,0 +1,38 @@
+﻿public class Colectivo
+{
+    private string linea;
+    private string empresa;
+    private const decimal TARIFA_BASICA = 1580m;
+
+    public Colectivo(string linea, string empresa)
+    {
+        this.linea = linea;
+        this.empresa = empresa;
+    }
+
+    public Boleto PagarCon(Tarjeta tarjeta)
+    {
+        if (tarjeta == null)
+            return null;
+
+        if (tarjeta.Saldo < TARIFA_BASICA)
+            return null;
+
+        if (tarjeta.PagarPasaje())
+        {
+            return new Boleto(TARIFA_BASICA, linea, empresa, tarjeta.Saldo);
+        }
+
+        return null;
+    }
+
+    public string Linea
+    {
+        get { return linea; }
+    }
+
+    public string Empresa
+    {
+        get { return empresa; }
+    }
+}
