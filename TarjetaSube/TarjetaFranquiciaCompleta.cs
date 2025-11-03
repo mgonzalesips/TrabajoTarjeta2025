d9cb781a6d27d5635b4fb72aadb4d1596ea26973
valenll <luchini.valu@gmail.com>
2025-11-02 23:11:34 -0300

    Informacion de boleto nueva agregada y testeada

--- TarjetaSube/TarjetaFranquiciaCompleta.cs
+++ TarjetaSube/TarjetaFranquiciaCompleta.cs
@@ -2,8 +2,6 @@ public class TarjetaFranquiciaCompleta : Tarjeta
 {
     public override bool PagarPasaje()
     {
-        // Los viajes son gratuitos, no se descuenta saldo
-        // Solo actualizamos la fecha del último viaje
         ultimoViaje = System.DateTime.Now;
         return true;
     }
